180ff8f0236243bc3787ccf546bde6476e24ac77
curlx <ninja075@gmail.com>
2023-11-28 11:14:55 +0100

    handle libraries and plugins with Gradle Version Catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,9 @@
-import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
-
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.dagger.hilt)
     kotlin("kapt")
-    kotlin("plugin.serialization")
-    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -52,44 +50,32 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    // Constraint Layout
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
-    // Hilt
-    implementation("com.google.dagger:hilt-android:2.48.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
-    // Ktor
-    implementation("io.ktor:ktor-client-android:2.1.3")
-    implementation("io.ktor:ktor-client-content-negotiation:2.1.3")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
-    // DateTime
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.1")
-    // Coil
-    implementation("io.coil-kt:coil-compose:2.5.0")
-    // Logger
-    implementation("com.jakewharton.timber:timber:5.0.1")
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.compose)
+    implementation(libs.constraintlayout.compose)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    implementation(libs.bundles.ktor)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.coil.compose)
+    implementation(libs.timber)
 
     // Testing
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("com.willowtreeapps.assertk:assertk:0.27.0")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:5.1.0")
-    testImplementation("app.cash.turbine:turbine:1.0.0")
+    testImplementation(libs.junit)
+    testImplementation(libs.assertk)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.turbine)
 
     // UI Testing
-    androidTestImplementation("io.github.kakaocup:compose:0.3.0")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    androidTestImplementation(libs.kakao.compose)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
